a989242b12ca5fa4a1e52a7efaaf8014a4bb6245
jayden2013 <jay-weav@hotmail.com>
2017-03-27 23:19:45 -0600

    Laying foundation for random photo and album mining.

---
+++ ErraticMiner.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Generates Random Imgur Links
+ * @author Jayden Weaver
+ *
+ */
+public class ErraticMiner {
+	String imgurURL = "http://imgur.com/";
+
+	public ErraticMiner(){
+
+	}
+
+	/**
+	 * Mines for URLs, and returns arrayList of URLs.
+	 * @param numSwings
+	 * @return
+	 */
+	public ArrayList<String> startMining(int numSwings){
+		ArrayList<String> linkArray = new ArrayList<String>();
+		for (int i = 0; i < numSwings; i++){
+			ImgurLink link = new ImgurLink(this.imgurURL + generateContentID());
+			if (link.isValid()){
+				linkArray.add(link.getURL());
+			}
+		}
+		return linkArray;
+	}	
+
+	/**
+	 * Generates and returns a random Content ID
+	 * @return
+	 */
+	private static String generateContentID(){
+		String possibleCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+		String contentID = "";
+		Random rand = new Random();
+
+		//Most content IDs are either 5 or 7 characters. Some six, but not many. 
+		if (rand.nextInt(2) == 0){ //if random number is zero, we'll use a 5 character ID
+			for (int i = 0; i < 5; i++){
+				contentID += possibleCharacters.charAt(rand.nextInt(possibleCharacters.length()));
+			}
+		}
+		else{ //else we'll use a 7 character ID.
+			for (int i = 0; i < 7; i++){
+				contentID += possibleCharacters.charAt(rand.nextInt(possibleCharacters.length()));
+			}	
+		}		
+		return contentID;
+	}
+}
---
+++ ImgurLink.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+/**
+ * Object to store information about imgur links
+ * @author Jayden Weaver
+ *
+ */
+public class ImgurLink {
+
+	String URL = "";
+	boolean isValid = false;
+	URL link;
+	InputStream inStream;
+	BufferedReader bufRead;
+
+	public ImgurLink(String url) { 
+		this.URL = url;
+		checkValidity();
+	}
+
+	private void checkValidity(){
+		try {
+			this.link = new URL(this.URL);
+			this.inStream = link.openStream();
+			this.bufRead = new BufferedReader(new InputStreamReader(this.inStream));
+			this.isValid = true;
+		} catch (IOException e) {
+			this.isValid = false;
+		}
+	}
+
+	/**
+	 * Sets the URL to any string.
+	 * @param url
+	 */
+	public void setURL(String url){
+		this.URL = url;
+	}
+
+	/**
+	 * Returns the associated URL
+	 * @return
+	 */
+	public String getURL(){
+		return this.URL;
+	}
+
+	/**
+	 * Returns true if it contains a valid URL
+	 * @return
+	 */
+	public boolean isValid(){	
+		return this.isValid;
+	}
+}
